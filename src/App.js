f23a87fd5e073afaf309fe38c95f9e735936c942
Rabindra Joshi <therj@users.noreply.github.com>
2018-06-26 23:00:28 +0545

    view, delete, css :zap:

--- src/App.js
+++ src/App.js
@@ -1,12 +1,57 @@
 import React, { Component } from 'react'
 import './App.css'
 import TodoList from './TodoList'
+import TodoItems from './TodoItems'
 
 class App extends Component {
+  inputElement = React.createRef()
+  constructor() {
+    super()
+    this.state = {
+      items: [],
+      currentItem: {
+        text: '',
+        key: '',
+      },
+    }
+  }
+  deleteItem = key => {
+    const filteredItems = this.state.items.filter(item => {
+      return item.key !== key
+    })
+    this.setState({
+      items: filteredItems,
+    })
+  }
+
+  handleInput = e => {
+    const itemText = e.target.value
+    const currentItem = { text: itemText, key: Date.now() }
+    this.setState({
+      currentItem,
+    })
+  }
+  addItem = e => {
+    e.preventDefault()
+    const newItem = this.state.currentItem
+    if (newItem.text !== '') {
+      const items = [...this.state.items, newItem]
+      this.setState({
+        items: items,
+        currentItem: { text: '', key: '' },
+      })
+    }
+  }
   render() {
     return (
       <div className="App">
-        <TodoList />
+        <TodoList
+          addItem={this.addItem}
+          inputElement={this.inputElement}
+          handleInput={this.handleInput}
+          currentItem={this.state.currentItem}
+        />
+        <TodoItems entries={this.state.items} deleteItem={this.deleteItem} />
       </div>
     )
   }
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
 
 class TodoList extends Component {
+  componentDidUpdate() {
+    this.props.inputElement.current.focus()
+  }
   render() {
     return (
       <div className="todoListMain">
         <div className="header">
-          <form>
-            <input placeholder="Task" />
+          <form onSubmit={this.props.addItem}>
+            <input
+              placeholder="Task"
+              ref={this.props.inputElement}
+              value={this.props.currentItem.text}
+              onChange={this.props.handleInput}
+            />
             <button type="submit"> Add Task </button>
           </form>
         </div>
